6ab761fa16343c986e68b5c4c3ac165c1d7f2d4b
kyleng <ng.kyle101@gmail.com>
2021-12-18 17:28:43 -0800

    Cleanup formatting

--- Main/Tests/CV_Tester.java
+++ Main/Tests/CV_Tester.java
@@ -6,13 +6,10 @@ import java.util.Scanner;
 
 public class CV_Tester {
     public static void main(String[] args){
-
         Scanner scanner = new Scanner(System.in);
         Pipeline pipeline = new Pipeline(scanner,
                 "/Users/kyleng/IdeaProjects/OpenCV_Regression/Main/Resources/Capstone.png"
         );
-
         pipeline.tuneFilterManually();
-
     }
 }
